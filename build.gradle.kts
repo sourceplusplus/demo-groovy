3f465466a51260ee0ba5be4cb031a6997e929295
Brandon Fergerson <bfergerson@apache.org>
2023-05-04 12:35:39 -0500

    build: bump probe version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("org.codehaus.groovy:groovy:3.0.13")
 
     //attaches Source++ probe to the application
-    javaagent("plus.sourceplus.probe:probe-jvm:0.7.7.1")
+    javaagent("plus.sourceplus.probe:probe-jvm:0.7.8-SNAPSHOT")
 
     //used for the tail logs command
     implementation("org.apache.skywalking:apm-toolkit-logback-1.x:8.15.0")
