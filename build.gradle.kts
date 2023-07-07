5b54f1f444171338180f2797d826ab2b3518d65b
Brandon Fergerson <bfergerson@apache.org>
2023-07-06 20:07:17 -0500

    chore: testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("groovy")
     id("application")
-    id("com.ryandens.javaagent-application") version "0.3.2"
+    id("com.ryandens.javaagent-application") version "0.4.2"
+    id("com.ryandens.javaagent-test") version "0.4.2"
 }
 
 group = "spp.demo"
@@ -28,6 +29,11 @@ dependencies {
     //used for endpoint indicators
     implementation("io.micronaut:micronaut-http-server-netty:3.6.4")
     implementation("io.micronaut:micronaut-inject-groovy:3.6.4")
+
+    //used for test indicators
+    testJavaagent("plus.sourceplus.probe:probe-jvm:0.7.9-SNAPSHOT")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 application {
