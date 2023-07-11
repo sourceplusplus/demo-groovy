19d9197684981b8dedc39db8534cc7b16abe03b1
Brandon Fergerson <bfergerson@apache.org>
2023-07-10 20:17:38 -0500

    build: bump probe

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     implementation("io.micronaut:micronaut-management:3.6.4")
 
     //attaches Source++ probe to the application
-    javaagent("plus.sourceplus.probe:probe-jvm:0.7.9-SNAPSHOT")
+    javaagent("plus.sourceplus.probe:probe-jvm:0.7.10-SNAPSHOT")
 
     //used for the tail logs command
     implementation("org.apache.skywalking:apm-toolkit-logback-1.x:8.15.0")
@@ -30,7 +30,7 @@ dependencies {
     implementation("io.micronaut:micronaut-inject-groovy:3.6.4")
 
     //used for test indicators
-    testJavaagent("plus.sourceplus.probe:probe-jvm:0.7.9-SNAPSHOT")
+    testJavaagent("plus.sourceplus.probe:probe-jvm:0.7.10-SNAPSHOT")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
